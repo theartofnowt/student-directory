620cbe4a184eac4b846b65e3643aa0fb86cc89e0
theartofnowt <michaelgordon.brown1@gmail.com>
2017-06-22 12:15:37 +0100

    Asking for User Input

--- directory.rb
+++ directory.rb
@@ -1,27 +1,29 @@
 
-#let's put all students into hashes within an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :noverber}
-]
+#asking for user imput
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  #create an empty array
+  students = []
+  #get the first name
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    #add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    #get another name from the user
+    name = gets.chomp
+  end
+  #return the array of students
+  students
+end
 
-#create a header
 def print_header
-  puts "The Students of Villains Academy"
+  puts "The Students of Villians Accdemy"
   puts "-----------"
 end
 
-#we wanted to clarify the previous code 'studen[0]' is it a name or what?
-#in order to clarify this we use hashes to define what is what. e.g student[:name]
 def print(students)
   students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
@@ -32,7 +34,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
-#nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
